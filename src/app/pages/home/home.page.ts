9bdd49dd2456f6966c538ca01bacb0812aa488b2
celine <amandaceline70@gmail.com>
2023-11-28 12:30:01 +1100

    create instagram feed placement

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -6,6 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  expand: Boolean = false;
+  imgSource: String = 'assets/poster.jpeg';
+  description: String = `
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+  do eiusmod tempor incididunt ut labore et dolore magna
+  aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+  ullamco laboris nisi ut aliquip ex ea commodo consequat.
+  Duis aute irure dolor in reprehenderit in voluptate velit
+  esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+  occaecat cupidatat non proident, sunt in culpa qui officia
+  deserunt mollit anim id est laborum.
+  `;
+
+  customCounterFormatter(inputLength: number, maxLength: number) {
+    return `${maxLength - inputLength} characters remaining`;
+  }
 
   constructor() { }
 
